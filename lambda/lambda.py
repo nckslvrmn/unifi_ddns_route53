115661a776594ba288f9f695ba48308be3dbbf07
Nick Silverman <nckslvrmn@gmail.com>
2025-02-10 17:00:32 -0500

    ruff formatting

--- lambda/lambda.py
+++ lambda/lambda.py
@@ -5,49 +5,57 @@ import boto3
 
 
 def authenticated(event):
-    if event.get('headers', {}).get('authorization') is None:
+    if event.get("headers", {}).get("authorization") is None:
         return False
 
-    creds = boto3.client('ssm').get_parameter(Name='/unifi_ddns_route53_credentials', WithDecryption=True)['Parameter']['Value']
+    creds = boto3.client("ssm").get_parameter(
+        Name="/unifi_ddns_route53_credentials", WithDecryption=True
+    )["Parameter"]["Value"]
 
-    auth = base64.b64decode(event['headers']['authorization'].split(' ')[-1])
-    return auth.decode('utf-8') == creds
+    auth = base64.b64decode(event["headers"]["authorization"].split(" ")[-1])
+    return auth.decode("utf-8") == creds
 
 
 def handler(event, _):
-    response = {'statusCode': 404, 'headers': {'Content-Type': 'text/plain'}, 'body': ''}
+    response = {
+        "statusCode": 404,
+        "headers": {"Content-Type": "text/plain"},
+        "body": "",
+    }
 
     if not authenticated(event):
         return response
 
     if any(
         (
-            (event.get('queryStringParameters', {}).get('hostname') is None),
-            (event.get('queryStringParameters', {}).get('myip') is None),
+            (event.get("queryStringParameters", {}).get("hostname") is None),
+            (event.get("queryStringParameters", {}).get("myip") is None),
         )
     ):
         return response
 
     try:
-        boto3.client('route53').change_resource_record_sets(
-            HostedZoneId=os.getenv('HOSTED_ZONE_ID'),
+        boto3.client("route53").change_resource_record_sets(
+            HostedZoneId=os.getenv("HOSTED_ZONE_ID"),
             ChangeBatch={
                 "Comment": "dynamic dns update",
                 "Changes": [
                     {
-                        "Action": 'UPSERT',
+                        "Action": "UPSERT",
                         "ResourceRecordSet": {
-                            "Name": event['queryStringParameters']['hostname'],
+                            "Name": event["queryStringParameters"]["hostname"],
                             "Type": "A",
                             "TTL": 60,
-                            "ResourceRecords": [{"Value": event['queryStringParameters']['myip']}],
+                            "ResourceRecords": [
+                                {"Value": event["queryStringParameters"]["myip"]}
+                            ],
                         },
                     }
                 ],
             },
         )
-        response['statusCode'] = 200
-        response['body'] = 'good'
+        response["statusCode"] = 200
+        response["body"] = "good"
     except:
         return response
 
